a991b425155f8db6820d709adc483d9ec1f2b2da
IzmaShahid <pss.10807.izma@gmail.com>
2024-08-11 16:55:08 +0500

    Chapter 3

---
+++ script.js
@@ -0,0 +1,23 @@
+// Assign your age into varaible
+var age = 14 ;
+alert("I am "+ age +" years old");
+
+// Assign visitcount
+let visitcount = 20 ;
+alert("You have visited this site "+ visitcount +" times");
+
+// declare varaible for your birthyear
+let birthyear = 2010 ;
+// create a message to display
+let message1 = `My Birthyear is ${birthyear}. <br/>`;
+// display the message
+document.write(message1);
+
+let message2 = (` Data type of my Declared variable is number.`);
+document.write(message2);
+
+let visitorname = JohnDoe;
+let producttitle = T-shirt;
+let quantity = 5;
+let message = `${visitorname} ordered ${quantity} ${producttitle} on XYZ Clothing Store.`;
+document.write(message);
